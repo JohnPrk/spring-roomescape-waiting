239ec3961b7948747d6511e286280320d8825477
JohnPrk <uo3641493@gmail.com>
2024-06-28 17:03:09 +0900

    fix : @Repository 삭제(MemberRepositoryCustom 구현체가 이미 @Repository를 가지고 있음)

--- src/main/java/roomescape/domain/member/domain/repository/MemberJpaRepository.java
+++ src/main/java/roomescape/domain/member/domain/repository/MemberJpaRepository.java
@@ -1,13 +1,11 @@
 package roomescape.domain.member.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import roomescape.domain.member.domain.Member;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface MemberJpaRepository extends MemberRepositoryCustom, JpaRepository<Member, Long> {
 
     Optional<Member> findByEmailAndPassword(String email, String password);
